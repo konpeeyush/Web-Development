c5af1a29a3d0e5b29eacb77f1fbc118afddfaf57
konpeeyush <piyushksingh2589@gmail.com>
2022-06-29 15:57:01 +0530

    Lesson 10

--- ReactTutorials/piyushblog/src/Home.js
+++ ReactTutorials/piyushblog/src/Home.js
@@ -1,33 +1,18 @@
 import { useState } from 'react';
 const Home = () => {
-    // let name='mario' ;
-    const[name,setName]=useState('mario');
-    const[age,setAge]=useState(20);
-    const handleClick = () => {
-        // console.log("Hello Ninjas")
-        // name="luigi" ;
-        // console.log(name); 
-        // name changes is reflected in the console but not in the DOM, as the DOM is not updated
-
-        setName('luigi');
-        setAge(30);
-    }
-    const handleClick2 = (name) => {
-        console.log("hello " + name);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'joshua', id: 3 }
+    ]);
     return (
-        // create a div with className "home"
         <div className="home">
-            {/* create a h2 with text "HomePage" */}
-            <h2>HomePage</h2>
-            <p>{name} is {age} years old.</p> {/* does not change name on click */}
-
-            <button onClick={handleClick}>Click Me</button>
-
-            {/* use anonymous function to invoke function with parameters */}
-            <button onClick={() => {
-                handleClick2("Piyush");
-            }}>Click Me again</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
