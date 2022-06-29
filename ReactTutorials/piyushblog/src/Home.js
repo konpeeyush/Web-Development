67c8449ac384ba36945f65656b5860b041691df1
konpeeyush <piyushksingh2589@gmail.com>
2022-06-29 16:30:39 +0530

    Lesson 13
    Instead of handling delete function in blogList component we have used setState as to handle delete in blogList we were interfering the values of props.

--- ReactTutorials/piyushblog/src/Home.js
+++ ReactTutorials/piyushblog/src/Home.js
@@ -6,11 +6,14 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" /> {/* <-- Implementation of props, it enables us to use data from different component */}
-
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario Blogs" />
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} /> {/* <-- Implementation of props, it enables us to use data from different component */}
         </div>
 
     );
