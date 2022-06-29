bae7d6ed03e54de6d6873da03cc11f251e27dd45
konpeeyush <piyushksingh2589@gmail.com>
2022-06-29 16:44:08 +0530

    Lesson 14 and 15
    Implementation of useEffect

--- ReactTutorials/piyushblog/src/Home.js
+++ ReactTutorials/piyushblog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -7,13 +7,24 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const[name,setName] = useState('mario');
+
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name) ;
+    }, [name]);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} /> {/* <-- Implementation of props, it enables us to use data from different component */}
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={()=>setName('luigi')}>Change Name</button>
+            <p>{name}</p>
         </div>
 
     );
