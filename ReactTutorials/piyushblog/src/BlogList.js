30b57a80c9af0360ac0bdf098960fc9ae8467578
konpeeyush <piyushksingh2589@gmail.com>
2022-06-30 18:33:44 +0530

    Lesson 17 and 18
    Learning fetch and using a loading component.

--- ReactTutorials/piyushblog/src/BlogList.js
+++ ReactTutorials/piyushblog/src/BlogList.js
@@ -1,13 +1,13 @@
-const BlogList = ({blogs,title,handleDelete}) => {
-    
-    return ( 
+const BlogList = ({ blogs, title, handleDelete }) => {
+
+    return (
         <div className="blog-list">
-        <h1>{title}</h1>
+            <h1>{title}</h1>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
-                    <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
