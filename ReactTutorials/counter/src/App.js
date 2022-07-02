060e070cde9b977364a98469dfb16675dfd99e6b
konpeeyush <piyushksingh2589@gmail.com>
2022-07-02 13:57:39 +0530

    Counter App Done!
    I learnt about useState basics.

--- ReactTutorials/counter/src/App.js
+++ ReactTutorials/counter/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-
+import { useState, useEffect } from 'react';
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-
+      <h1 className={count > 0 ? "positive" : count < 0 ? "negative" : null}>{count}</h1>
+      <div className="button__wrapper">
+        <button className='btn' onClick={() => setCount(count - 1)}>-</button>
+        <button className='btn' onClick={() => setCount(count + 1)}>+</button>
+      </div>
     </div>
   );
 }
